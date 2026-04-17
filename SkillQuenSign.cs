943704c3b2c17e9387f37243f8c9b691f8597196
Coober-Ding <38315601+Coober-Ding@users.noreply.github.com>
2026-04-18 03:14:45 +0800

    昆恩印汇编修复

--- SkillQuenSign.cs
+++ SkillQuenSign.cs
@@ -286,22 +286,29 @@ public partial class TheWitcher : Mod
             .Save();
 
         // Spell attack
+        /**
+         
+         with (scr_instance_exists_in_list(o_b_magical_shield, _target.buffs))
+            should_execute = true
+         */
         Msl.LoadAssemblyAsString("gml_GlobalScript_scr_skill_damage")
             .MatchFrom("pop.v.i local.dmg")
-            .InsertBelow(@"push.v arg.argument0
+            .InsertBelow(@"push.v self.target
 pushi.e -9
 push.v [stacktop]self.buffs
 pushi.e o_b_magical_shield
+conv.i.v
 call.i gml_Script_scr_instance_exists_in_list(argc=2)
+pushi.e -9
 pushenv [1096]
 
 :[1094]
 pushi.e 1
 pop.v.b self.should_execute
-bf [1096]
 
 :[1096]
-popenv [1094]")
+popenv [1094]
+")
             .Save();
 
         // Arrow attack
@@ -328,49 +335,7 @@ popenv [1094]")
 
                 // Finish execution
 
-/* TODO
-                Msl.LoadAssemblyAsString("gml_GlobalScript_scr_damage_calculation")
-                    .MatchFrom("pop.v.v self.is_deal_damage")
-                    .InsertBelow(@"pushbltn.v builtin.argument0
-        pushi.e -9
-        push.v [stacktop]self.buffs
-        pushi.e o_b_magical_shield
-        conv.i.v
-        call.i gml_Script_scr_instance_exists_in_list(argc=2)
-        pushi.e -9
-        pushenv [1097]
 
-        :[1095]
-        pushi.e 0
-        pop.v.b self.should_execute
-        pushi.e 0
-        conv.i.v
-        pushi.e 4
-        conv.i.v
-        pushi.e snd_block_1
-        conv.i.v
-        pushi.e snd_block_2
-        conv.i.v
-        pushi.e snd_block_3
-        conv.i.v
-        pushi.e snd_block_4
-        conv.i.v
-        call.i choose(argc=4)
-        call.i audio_play_sound(argc=3)
-        popz.v
-        push.v self.Shield_Duration
-        pushi.e 0
-        cmp.i.v LTE
-        bf [1097]
-
-        :[1096]
-        call.i instance_destroy(argc=0)
-        popz.v
-
-        :[1097]
-        popenv [1095]")
-                    .Save();
-*/
 
 
                 // Throwed net
