9dfcf50edb7d52cab256828e6fff65f5c5a3010b
Coober-Ding <38315601+Coober-Ding@users.noreply.github.com>
2026-04-18 18:49:36 +0800

    加一点注释

--- Perks.cs
+++ Perks.cs
@@ -200,7 +200,7 @@ popenv [44]")
             new MslEvent(eventType: EventType.Other, subtype: 10, code: @"
                 event_inherited()
             "),
-
+            // 暴击加闪避
             new MslEvent(eventType: EventType.Other, subtype: 11, code: @"
                 if (attack_result == ""crit"")
                 {
@@ -209,7 +209,7 @@ popenv [44]")
                     scr_temp_effect_update(object_index, o_player, ""Block_Power"", 5, 3, 5)
                 }
             "),
-
+            // 闪避格挡加暴击
             new MslEvent(eventType: EventType.Other, subtype: 12, code: @"
                 if ((attack_result == ""block"") || (attack_result == ""fumbleBlock"")
                         || (evade && (attack_result == ""miss"" || attack_result == ""fumble"")))
@@ -304,7 +304,7 @@ popenv [44]")
             ")
             .Save();
         */
-
+        // 标记闪避
         Msl.LoadGML("gml_GlobalScript_scr_attack")
             .MatchFromUntil(" if (_target_evs >= 0)", "_evade = true")
             .InsertBelow(@"
@@ -315,7 +315,7 @@ popenv [44]")
                     }
             ")
             .Save();
-
+        // 面对远程投射物，可以用自己的精准度来格挡
         Msl.LoadGML("gml_GlobalScript_scr_attack_shot_block_chance")
             .MatchFrom("_isBlock = scr_chance_value(PRR)")
             .InsertBelow(@"
